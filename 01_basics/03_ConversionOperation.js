2319b1c3348ab6b26261f643bd612e635e8c6db1
Nishant Kudale <148056508+nishant22-07@users.noreply.github.com>
2024-11-15 11:46:29 +0000

    conversion

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = true ;
+
+
+console.log(typeof score);
+console.log(typeof (score));
+let vlaueInNNumber = Number(score);
+console.log(typeof vlaueInNNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0 ;
+
+
+let isLoggedIn = null;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 => true;
+// "Nishant" => true ;
+// 0 => false ;
+// null => false ;
+// "" => false ;
+
+let number = 33;
+let stringNumber = String(Number);
+console.log(typeof number);
+console.log(typeof(stringNumber));
+
