5cc3a9a8ff5583aeb2394aecceeeee1ecf4d71ee
nishant <nishantkudale04@gmail.com>
2025-06-25 17:15:01 +0530

    add initial array examples and methods in JavaScript

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,49 @@
+// arrays
+
+const arr = [1,2,3,4,5,6];
+const myHeros = ["spiderMan","ironMan","batMan"]
+// console.log(arr);
+// console.log(myHeros)
+
+// const arr2 = new Array(1,2,3,4,4,5)
+// console.log(arr2);
+
+// Array methods
+
+
+// arr.push(7)
+// arr.push(8)
+// arr.pop()
+
+// arr.unshift(9) // adds 9 at beginning and shifts all elements
+// arr.shift() // removes first element
+
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(3));
+
+
+const newArr = arr.join()
+// console.log(arr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+// slice or splice
+const myArr = [0,1,2,3,4,5,6,7];
+console.log("A", myArr);
+
+const  myn1 = myArr.slice(1,4)
+console.log("B",myn1);
+
+const myn2 = myArr.splice(1,4);
+console.log("C",myn2);
+
+console.log();
+console.log("D",myArr);
+
+
+
+
+
+
+
