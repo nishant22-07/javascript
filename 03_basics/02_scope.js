573ce0e01ab62b152dece4631125f4efa9260ad0
nishant <nishantkudale04@gmail.com>
2025-06-29 22:35:31 +0530

    refactor: comment out console log statements for clarity and maintainability

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,11 +5,58 @@ if (true) {
     let a = 10;
     const b = 20;
     var c = 30;
-    console.log("inner a:",a );
+    // console.log("inner a:",a );
     
     
     
 }
-    console.log(a);
+    // console.log(a);
     // console.log(b);
     // console.log(c);
+
+function one() {
+    const username = "Nishant";
+
+    function two() {
+        const website = " Youtube ";
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two();
+}
+// one();
+
+// if (true) {
+//     const username = " Kanha";
+//     if (username=== " Kanha") {
+//         let id = 22072004;
+//         console.log(id + username);
+//     }
+//     // console.log(id);
+// }
+
+// console.log(username);//no scope of username here
+
+
+
+
+//++++++++++++++++++++++++++++++ Interesting  +++++++++++++++++++++++++++++++++++++++
+
+
+
+// console.log(addOne(6));  //Cannot access 'addtwo' before initialization
+
+console.log(addtwo(5));
+function addOne(num) {
+    return num +1 ;
+}
+
+
+const addtwo = function(num) {
+    return num + 2 ;
+}
+
+
+
+
