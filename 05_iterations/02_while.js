9e1b54107899c7a10c380df03a7203ef7dd881b1
nishant <nishantkudale04@gmail.com>
2025-07-01 15:30:31 +0530

    add while and do while examples with comments

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,22 @@
+// while and do while 
+
+let i = 0;
+// while (i<=10) {
+//     console.log(i);
+//     i=i+2;
+// }
+
+let arr =  [ "Spiderman", "Superman", "Ironman"];
+let  index= 0
+// while (index<arr.length) {
+//     console.log(arr[index]);
+//     index++
+// }
+
+let score = 11;
+
+do {
+    console.log(score);
+    score++;
+    
+} while (score<= 10);
