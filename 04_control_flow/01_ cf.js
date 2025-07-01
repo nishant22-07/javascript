1d816058f58aa9cb38c2665e5b28e0c4662c649d
nishant <nishantkudale04@gmail.com>
2025-07-01 10:38:46 +0530

    add control flow examples with temperature and balance checks

---
+++ 04_control_flow/01_ cf.js
@@ -0,0 +1,46 @@
+// if
+
+const isUserLoggedIn= true
+const temp = 65;
+
+if (temp<50) {
+    console.log("temperature is less than 50");
+}else{
+    console.log("temperature is greater than 50");
+}
+
+
+// if (false)) {
+    
+// }
+
+if (2==="2") {
+    console.log("excuted");
+}
+
+// <, >, <=, >=, ==, !=, ===
+const score = 100;
+// if (score>100) {
+//     const power = "fly";
+//     console.log(`User power is ${power}`);
+// }
+    
+// console.log(`User power is ${power}`);
+
+const balance = 1000;
+// NOT GOOD METHOD
+// if (balance>500) console.log("test"),console.log(test2);
+
+// if (balance<500) {
+//     console.log("less than 500");
+// }else if (balance<750) {
+//     console.log("less than 750");
+    
+// }else if (balance<900) {
+//     console.log("less than 900");
+    
+// }else if (balance<1200) {
+//     console.log("less than 1200");
+    
+// }
+    
